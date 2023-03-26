ad61240910edb2e7756651f0ef8ac65569b6bf6b
antonlukichev14 <antonlukichev14@gmail.com>
2023-03-26 12:12:32 +1000

    Some Changes

--- GameObject.cs
+++ GameObject.cs
@@ -11,6 +11,8 @@ namespace SpaceBattle2128
         public Vector2 position;
         public byte renderID;
         public string tag;
+        public byte z; //Перекрытие при рендере
+        public bool collider; //Могут ли находится другие объекты на этой клетке. True - не могут.
 
         public abstract void Update();
     }
---
+++ GameSceneRender.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceBattle2128
+{
+    static class GameSceneRender
+    {
+        static public void Render()
+        {
+
+        }
+    }
+}
