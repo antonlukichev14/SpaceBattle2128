75003197eeb3bf897b388efc43f9ac42322ee1c0
antonlukichev14 <128899922+antonlukichev14@users.noreply.github.com>
2023-04-01 23:23:09 +1000

    Update Actor.cs

--- Actor.cs
+++ Actor.cs
@@ -11,6 +11,7 @@
                 if (position.x + direction.x > 0 && position.x + direction.x < GameScene.currentGameScene.grid.tiles.GetLength(0) && position.y + direction.y > 0 && position.y + direction.y < GameScene.currentGameScene.grid.tiles.GetLength(1))
                 {
                     //Изменяет положение в соответсвующем направлении
+                    GameScene.currentGameScene.grid.tiles[position.x, position.y].currentObject = null;
                     GameScene.currentGameScene.grid.tiles[position.x += direction.x, position.y += direction.y].currentObject = this;
                 }
             }
