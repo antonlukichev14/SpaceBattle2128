8d777ba2fa08db87ebe019ef242fb103de845982
antonlukichev14 <128899922+antonlukichev14@users.noreply.github.com>
2023-04-01 23:25:03 +1000

    Update Player.cs

--- Player.cs
+++ Player.cs
@@ -4,55 +4,33 @@ namespace SpaceBattle2128
 {
     class Player : Actor
     {
-        GameObject player = null; // не понял откуда брать player
         public override void Update()
         {
             void Move()
             {
-                Input.SetKey();
                 switch (Input.GetKey())
                 {
                     case ConsoleKey.W:
                         {
-                            if (GameScene.currentGameScene.grid.tiles[position.x, position.y - 1].wall == false)
-                            {
-                                GameScene.currentGameScene.grid.tiles[position.x, position.y].currentObject = null;
-                                position.y--;
-                                GameScene.currentGameScene.grid.tiles[position.x, position.y].currentObject = player;
-                            }
+                            //Через Move()
                         }
                         break;
 
                     case ConsoleKey.S:
                         {
-                            if (GameScene.currentGameScene.grid.tiles[position.x, position.y + 1].wall == false)
-                            {
-                                GameScene.currentGameScene.grid.tiles[position.x, position.y].currentObject = null;
-                                position.y++;
-                                GameScene.currentGameScene.grid.tiles[position.x, position.y].currentObject = player;
-                            }
+                            //Через Move()
                         }
                         break;
 
                     case ConsoleKey.A:
                         {
-                            if (GameScene.currentGameScene.grid.tiles[position.x - 1, position.y].wall == false)
-                            {
-                                GameScene.currentGameScene.grid.tiles[position.x, position.y].currentObject = null;
-                                position.x--;
-                                GameScene.currentGameScene.grid.tiles[position.x, position.y].currentObject = player;
-                            }
+                            //Через Move()
                         }
                         break;
 
                     case ConsoleKey.D:
                         {
-                            if (GameScene.currentGameScene.grid.tiles[position.x + 1, position.y].wall == false)
-                            {
-                                GameScene.currentGameScene.grid.tiles[position.x, position.y].currentObject = null;
-                                position.x++;
-                                GameScene.currentGameScene.grid.tiles[position.x, position.y].currentObject = player;
-                            }
+                            //Через Move()
                         }
                         break;
 
