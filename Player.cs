7255344809dd12808ffc20156d487c3340b1b74f
lilfrog4 <romansimonenko06@gmail.com>
2023-04-01 23:49:10 +1000

    Update Player.cs

--- Player.cs
+++ Player.cs
@@ -6,24 +6,29 @@ namespace SpaceBattle2128
     {
         public override void Update()
         {
+            Vector2 playerMove = new Vector2();
             switch (Input.GetKey())
-                {
-                    case ConsoleKey.W:
-                            //Через Move()
-                        break;
+            {
+                case ConsoleKey.W:
+                    playerMove.x = 0; playerMove.y = -1;
+                    Move(playerMove);
+                    break;
 
-                    case ConsoleKey.S:
-                            //Через Move()
-                        break;
+                case ConsoleKey.S:
+                    playerMove.x = 0; playerMove.y = 1;
+                    Move(playerMove);
+                    break;
 
-                    case ConsoleKey.A:
-                            //Через Move()
-                        break;
+                case ConsoleKey.A:
+                    playerMove.x = -1; playerMove.y = 0;
+                    Move(playerMove);
+                    break;
 
-                    case ConsoleKey.D:
-                            //Через Move()
-                        break;
-              }
+                case ConsoleKey.D:
+                    playerMove.x = 1; playerMove.y = 0;
+                    Move(playerMove);
+                    break;
+            }
         }
 
     }
