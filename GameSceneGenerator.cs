7a1fb72234627f8b663f9f922b8c7d19a9b783a2
AlexM2048 <trank03052004@gmail.com>
2023-04-02 22:30:29 +1000

    HotFix GenerateGameObjects()

--- GameSceneGenerator.cs
+++ GameSceneGenerator.cs
@@ -53,9 +53,9 @@ namespace SpaceBattle2128
                     if (tile.currentObject != null)
                     {
                         canPlace[x, y] = false;
-                        for (int j = 1; j < radius; j++)
+                        for (int j = 1; j <= radius; j++)
                         {
-                            for (int i = 1; i < radius; i++)
+                            for (int i = 1; i <= radius; i++)
                             {
                                 if (inRadius[i, j])
                                 {
@@ -79,9 +79,9 @@ namespace SpaceBattle2128
                         count--;
 
                         canPlace[x, y] = false;
-                        for (int j = 1; j < radius; j++)
+                        for (int j = 1; j <= radius; j++)
                         {
-                            for (int i = 1; i < radius; i++)
+                            for (int i = 1; i <= radius; i++)
                             {
                                 if (inRadius[i, j])
                                 {
