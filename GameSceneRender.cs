f29354c6bb6d19cd655b93eb93c02d02417014e7
lilfrog4 <128126326+lilfrog4@users.noreply.github.com>
2023-04-02 19:56:18 +1000

    Create ObjectList.cs

--- GameSceneRender.cs
+++ GameSceneRender.cs
@@ -1,15 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SpaceBattle2128
 {
     static class GameSceneRender
     {
-        static public void Render()
+        static public void Render(GameScene scene)
         {
+            for (int y = 0; y < scene.grid.height; y++)
+            {
+                for (int x = 0; x < scene.grid.width; x++)
+                {
+                    Console.Write(scene.grid.tiles[x, y]);
+                }
+                Console.WriteLine();
+            }
+
 
         }
     }
