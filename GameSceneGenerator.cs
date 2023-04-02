7e00d05c82b7f492f141ef94fbe726d4e342fe40
AlexM2048 <trank03052004@gmail.com>
2023-04-02 19:12:20 +1000

    Add GenerateSaveZone()

--- GameSceneGenerator.cs
+++ GameSceneGenerator.cs
@@ -25,19 +25,40 @@
             int width = 5;
             int height = 5;
 
+            int centreX = grid.width / 2;
+            int centreY = grid.height / 2;
+
             FloorObject saveZoneFloor = new FloorObject(floorID, tag);
             Wall saveZoneWall = new Wall(wallID, tag);
 
             for (int y = 0; y < height; y++)
                 for (int x = 0; x < width; x++)
                 {
-                    Tile tile = grid.tiles[x, y];
+                    Tile tile = grid.tiles[centreX + x, centreY + y];
+                    tile.wall = false;
 
                     if (x == 0 || y == 0 || x == (width - 1) || y == (height - 1))
                         tile.currentObject = saveZoneWall;
                     else
                         tile.currentFloorObject = saveZoneFloor;
                 }
+
+            centreX += (width / 2);
+
+            Tile door = grid.tiles[centreX, centreY];
+            door.currentObject = null;
+            door.currentFloorObject = saveZoneFloor;
+
+            for (int y = 1; true; y++)
+            {
+                Tile path = grid.tiles[centreX, centreY + y];
+
+                if (path.wall)
+                {
+                    path.wall = false;
+                    path.currentFloorObject = saveZoneFloor;
+                }                  
+            }
         }
     }
 }
