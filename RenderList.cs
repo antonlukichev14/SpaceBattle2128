2dfc499e50804e6b5ef57345bf604a3794293bc2
antonlukichev14 <antonlukichev14@gmail.com>
2023-04-02 21:03:34 +1000

    Create RenderList.cs

---
+++ RenderList.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceBattle2128
+{
+    static class RenderList
+    {
+        static Dictionary<byte, char> renderList = new Dictionary<byte, char>
+        {
+            { 0, ' ' },
+            { 1, '#' }
+        };
+
+        static char Get(byte renderID)
+        {
+            return renderList[renderID];
+        }
+    }
+}
