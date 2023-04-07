76331d1eeac9618ee01ce047ea4e0647b2287ba4
Alina1477 <alina.iv.al17@gmail.com>
2023-04-07 23:25:35 +1000

    Добавила телепорт

---
+++ Teleport.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceBattle2128
+{
+    internal class Teleport: Item
+    {
+        public override void Use()
+        {
+            Player player = GameScene.currentGameScene.player;
+            Grid grid = GameScene.currentGameScene.grid;
+            Vector2 playerPos = player.position;
+            Vector2 SZPos = GameScene.currentGameScene.savezonePosition;
+            grid.tiles[SZPos.x, SZPos.y].currentObject = player;
+            grid.tiles[playerPos.x, playerPos.y].currentObject = null;
+            playerPos = new Vector2(SZPos);
+        }
+    }
+}
