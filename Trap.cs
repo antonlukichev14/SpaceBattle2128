1458f418094b0825dd4affee672a7ad4c1c5b99a
Alina1477 <alina.iv.al17@gmail.com>
2023-04-14 00:23:04 +1000

    Добавила ловушку

---
+++ Trap.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceBattle2128
+{
+    internal class Trap
+    {
+    }
+}
